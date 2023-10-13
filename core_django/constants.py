a75977d7d1ff99266e51a87d125faa9776713da5
Arpit Patel <arpitpatel1771@gmail.com>
2023-10-13 06:52:33 +0530

    commited via a script

---
+++ core_django/constants.py
@@ -0,0 +1,9 @@
+UNAUTHORIZED_401_RESPONSE_DATA = {
+    "status": "ERROR",
+    "error": "Token either has invalid credentials or is generated from an invalid source"
+}
+
+FORBIDDEN_403_RESPONSE_DATA = {
+    "status": "ERROR",
+    "error": "User is not allowed to access the view"
+}
