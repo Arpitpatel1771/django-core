606bc7c30ec0d5e162ba40ecb0de32bbb30511ea
Arpit Patel <arpitpatel1771@gmail.com>
2023-10-13 07:24:27 +0530

    commited via a script

--- core_django/auth/decorators.py
+++ core_django/auth/decorators.py
@@ -1,14 +1,16 @@
 from functools import wraps
-from rest_framework.response import Response
+
 from rest_framework import status
+from rest_framework.response import Response
+
+from core_django.auth.utils import (
+    authorize_user_for_views,
+    validate_token_against_secret,
+)
 from core_django.constants import (
-    UNAUTHORIZED_401_RESPONSE_DATA,
     FORBIDDEN_403_RESPONSE_DATA,
     TOKEN_NOT_FOUND_401_RESPONSE_DATA,
-)
-from core_django.auth.utils import (
-    validate_token_against_secret,
-    authorize_user_for_views,
+    UNAUTHORIZED_401_RESPONSE_DATA,
 )
 
 
@@ -16,18 +18,18 @@ def authorize(groups=[]):
     def decorator(function):
         @wraps(function)
         def wrapper(request, *args, **kwargs):
-            auth_value: str = request.META.get("HTTP_AUTHORIZATION")
+            token: str = request.META.get("HTTP_AUTHORIZATION")
 
             # Check if a token exists in the request headers
-            if type(auth_value) != str or auth_value.strip() == "":
-                raise Response(
+            if type(token) != str or token.strip() == "":
+                return Response(
                     data=TOKEN_NOT_FOUND_401_RESPONSE_DATA,
                     status=status.HTTP_401_UNAUTHORIZED,
                 )
             else:
-                auth_value = auth_value.strip()
+                token = token.strip()
 
-            token = auth_value.split(" ")[-1]
+            token = token.split(" ")[-1]
 
             # Check if the token is valid by verifying it from UMS
             if not validate_token_against_secret(token):
