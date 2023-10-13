86ab173a0634ccd2311052ebe352bef4459c39ea
Arpit Patel <arpitpatel1771@gmail.com>
2023-10-13 06:44:39 +0530

    eod

--- core_django/models.py
+++ core_django/models.py
@@ -3,16 +3,18 @@ from django.db import models
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from django.core.serializers.json import DjangoJSONEncoder
 
-class LogEntryCustom(LogEntry):
 
-    object_data = models.JSONField(verbose_name="object_data", encoder=DjangoJSONEncoder)
+class LogEntryCustom(LogEntry):
+    object_data = models.JSONField(
+        verbose_name="object_data", encoder=DjangoJSONEncoder
+    )
 
     class Meta:
-        app_label = 'core_django'
+        app_label = "core_django"
+        db_table = "core_django_logentrycustom"
 
 
 class BaseModel(models.Model):
-
     status = models.BooleanField(default=True)
     creation_date = models.DateTimeField(auto_now_add=True)
     creation_by = models.BigIntegerField(null=True)
@@ -23,8 +25,6 @@ class BaseModel(models.Model):
         abstract = True
 
     def save(self, *args, **kwargs) -> None:
-        
         # TODO: make it so that all data which is changed gets stoored in LogEntryCustom
 
         return super().save(*args, **kwargs)
-    
